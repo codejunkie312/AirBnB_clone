b1884f4f3865c296ecc2472935e39117bf04137a
Lotizzle <lotharmbaegbu@gmail.com>
2023-11-09 14:32:00 -0800

    Added to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -5,22 +5,38 @@ from datetime import datetime
 
 
 class BaseModel:
-	"""This class defines all common attributes/methods for other classes"""
-	
-	def __init__(self):
-		"""Initializes a new user"""
+    """This class defines all common attributes/methods for other classes"""
 
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+    def __init__(self):
+        """Initializes a new user"""
 
-	def save(self):
-		"""Updates the updated_at attribute with the current datetime"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-		self.updated_at = datetime.now()
-	
-	def __str__(self):
-		"""Returns a string representation of the BaseModel instance"""
+    def save(self):
+        """Updates the updated_at attribute with the current datetime"""
 
-		class_name = self.__class__.__name__
-		return f"[{class_name}] ({self.id}) {self.__dict__}"
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the BaseModel instance"""
+
+        class_name = self.__class__.__name__
+        iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        custom_dict = {
+            **self.__dict__,
+            "__class__": class_name,
+            "id": self.id,
+            "created_at": self.created_at.strftime(iso_format),
+            "updated_at": self.updated_at.strftime(iso_format),
+        }
+
+        return custom_dict
+
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance"""
+
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
