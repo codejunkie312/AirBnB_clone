3893852daec5d3db279dc538beceb21ad0d0a518
Lotizzle <lotharmbaegbu@gmail.com>
2023-11-10 04:42:53 -0800

    Dict representation added

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,21 @@ from datetime import datetime
 class BaseModel:
     """This class defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new user"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
