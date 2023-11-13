03ad696040d32314ab52923557dec2e9d030340a
Lotizzle <lotharmbaegbu@gmail.com>
2023-11-12 16:41:34 -0800

    Added some tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines unittests for base_model.py."""
+import unittest
+import uuid
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """unittests for testing the BaseModel methods"""
+
+    def setUp(self):
+        self.model = BaseModel()
+
+    def tearDown(self):
+        del self.model
+
+    def test_one_instance(self):
+        self.assertIsInstance(self.model, BaseModel)
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
+        self.assertEqual(type(self.model.id), str)
+        self.assertEqual(type(self.model.created_at), datetime)
+	self.assertEqual(type(self.model.updated_at), datetime)
+
+    def test_unique_id(self):
+        b = BaseModel()
+        self.assertNotEqual(self.model.id, b.id)
+
+    def test_id_type(self):
+        self.assertNotEqual(type(self.model.id), UUID)
