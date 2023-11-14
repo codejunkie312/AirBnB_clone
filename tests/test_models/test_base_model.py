b8e19f2bcc7bd45edd9a3857d9223a038cf34494
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-11-15 00:54:52 +0100

    added the tests for file storage class and baseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,38 +1,97 @@
 #!/usr/bin/python3
-"""Defines unittests for base_model.py."""
+"""This module defines the unittests for models/base_model.py"""
 import unittest
-import uuid
 from models.base_model import BaseModel
 from datetime import datetime
+import uuid
+import json
+import os
 
 
-class TestBaseModel_instantiation(unittest.TestCase):
-    """unittests for testing the BaseModel methods"""
+class TestBaseModel(unittest.TestCase):
+    """Unittests for testing the BaseModel class"""
 
     def setUp(self):
-        self.model = BaseModel()
+        """Sets up the testing environment"""
+        pass
 
     def tearDown(self):
-        del self.model
+        """Tears down the testing environment"""
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def test_one_instance(self):
-        self.assertIsInstance(self.model, BaseModel)
+    def test_init(self):
+        """Tests the initialization of the base model instance"""
+        my_model = BaseModel()
+        self.assertTrue(isinstance(my_model, BaseModel))
 
-    def test_attributes(self):
-        self.assertTrue(hasattr(self.model, 'id'))
-        self.assertTrue(hasattr(self.model, 'created_at'))
-        self.assertTrue(hasattr(self.model, 'updated_at'))
-        self.assertEqual(type(self.model.id), str)
-        self.assertEqual(type(self.model.created_at), datetime)
-        self.assertEqual(type(self.model.updated_at), datetime)
+    def test_init_id(self):
+        """Tests the id of the base model instance"""
+        my_model = BaseModel()
+        self.assertTrue(type(my_model.id), str)
 
-    def test_unique_id(self):
-        b = BaseModel()
-        self.assertNotEqual(self.model.id, b.id)
+    def test_init_created_at(self):
+        """Tests the created_at attribute of the base model instance"""
+        my_model = BaseModel()
+        self.assertTrue(type(my_model.created_at), datetime)
 
-    def test_id_type(self):
-        self.assertNotEqual(type(self.model.id), uuid.UUID)
+    def test_init_updated_at(self):
+        """Tests the updated_at attribute of the base model instance"""
+        my_model = BaseModel()
+        self.assertTrue(type(my_model.updated_at), datetime)
 
+    def test_init_kwargs(self):
+        """Tests the initialization of the base model instance with kwargs"""
+        my_model = BaseModel()
+        my_model.name = "ALX"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        my_new_model = BaseModel(**my_model_json)
+        self.assertTrue(isinstance(my_new_model, BaseModel))
+        self.assertTrue(my_new_model.name, "ALX")
+        self.assertTrue(my_new_model.my_number, 89)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_str(self):
+        """Tests the __str__ method of the base model instance"""
+        my_model = BaseModel()
+        expected = "[{}] ({}) {}".format(
+            my_model.__class__.__name__, my_model.id, my_model.__dict__)
+        actual = str(my_model)
+        self.assertEqual(expected, actual)
+
+    def test_save(self):
+        """Tests the save method of the base model instance"""
+        my_model = BaseModel()
+        my_model.save()
+        self.assertNotEqual(my_model.created_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        """Tests the to_dict method of the base model instance"""
+        my_model = BaseModel()
+        my_model.name = "ALX"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        expected = {
+            'id': my_model.id,
+            '__class__': my_model.__class__.__name__,
+            'created_at': my_model.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+            'updated_at': my_model.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+            'name': "ALX",
+            'my_number': 89
+        }
+        self.assertDictEqual(expected, my_model_json)
+
+    def test_to_dict_isoformat(self):
+        """Tests the to_dict method of the base model instance with isoformat
+        """
+        my_model = BaseModel()
+        my_model.name = "ALX"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        self.assertEqual(my_model_json["created_at"],
+                         my_model.created_at.isoformat())
+        self.assertEqual(my_model_json["updated_at"],
+                         my_model.updated_at.isoformat())
+        self.assertEqual(my_model_json["__class__"], "BaseModel")
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+"""This module containt the unittests for the FileStorage class"""
+import unittest
+import os
+import json
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+
+
+class TestFileStorage(unittest.TestCase):
+    """This class tests the FileStorage class"""
+    def setUp(self):
+        """Sets up the testing environment"""
+        if os.path.isfile("file.json"):
+            os.rename("file.json", "file.json.temp")
+        self.storage = FileStorage()
+        self.model = BaseModel()
+
+    def tearDown(self):
+        """Cleans up the testing environment after each test by removing the JSON
+        file created"""
+        del self.storage
+        del self.model
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+        if os.path.isfile("file.json.temp"):
+            os.rename("file.json.temp", "file.json")
+
+    def test_all(self):
+        """Test that all return the __objects dictionary"""
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+    
+    def test_new(self):
+        """Test that new adds an object to the __objects dictionary"""
+        self.storage.new(self.model)
+        key = self.model.__class__.__name__ + "." + self.model.id
+        self.assertIn(key, FileStorage._FileStorage__objects)
+
+    def test_save(self):
+        """Test that save serializes __objects to the JSON file (path: __file_path)"""
+        self.storage.new(self.model)
+        self.storage.save()
+        self.assertTrue(os.path.isfile(FileStorage._FileStorage__file_path))
+    
+    def test_reload(self):
+        """Test that reload deserializes the JSON file to __objects (only if the
+        JSON file (__file_path) exists; otherwise, do nothing. If the file doesn't
+        exist, no exception should be raised)"""
+        self.storage.new(self.model)
+        self.storage.save()
+        self.storage.reload()
+        key = self.model.__class__.__name__ + "." + self.model.id
+        self.assertIn(key, FileStorage._FileStorage__objects)
+    
+    def test_reload_no_file(self):
+        """Test that reload does not raise an exception if the JSON file does not
+        exist"""
+        self.storage.reload()
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+    
+    def test_reload_empty_file(self):
+        """Test that reload does not raise an exception if the JSON file is empty"""
+        with open(FileStorage._FileStorage__file_path, 'w') as f:
+            f.write("")
+        try:
+            self.storage.reload()
+        except:
+            pass
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+
+    def test_reload_invalid_file(self):
+        """Test that reload does not raise an exception if the JSON file is invalid"""
+        with open(FileStorage._FileStorage__file_path, 'w') as f:
+            f.write("{")
+        try:
+            self.storage.reload()
+        except:
+            pass
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+    
+    def test_reload_invalid_file_format(self):
+        """Test that reload does not raise an exception if the JSON file is invalid"""
+        with open(FileStorage._FileStorage__file_path, 'w') as f:
+            f.write("This is not a JSON file")
+        try:
+            self.storage.reload()
+        except:
+            pass
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+    
+    def test_reload_invalid_file_content(self):
+        """Test that reload does not raise an exception if the JSON file is invalid"""
+        with open(FileStorage._FileStorage__file_path, 'w') as f:
+            f.write("[]")
+        try:
+            self.storage.reload()
+        except:
+            pass
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+    
+    def test_reload_invalid_file_content2(self):
+        """Test that reload does not raise an exception if the JSON file is invalid"""
+        with open(FileStorage._FileStorage__file_path, 'w') as f:
+            f.write("[{}]")
+        try:
+            self.storage.reload()
+        except:
+            pass
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
+    
+    def test_reload_invalid_file_content3(self):
+        """Test that reload does not raise an exception if the JSON file is invalid"""
+        with open(FileStorage._FileStorage__file_path, 'w') as f:
+            f.write("[{}, {}]")
+        try:
+            self.storage.reload()
+        except:
+            pass
+        self.assertEqual(self.storage.all(), FileStorage._FileStorage__objects)
