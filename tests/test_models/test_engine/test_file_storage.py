41ed0b019c46e9e9953ac1ea190f7042ed61fa35
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-11-20 00:11:27 +0100

    added more tests to File Storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -62,7 +62,7 @@ class TestFileStorage(unittest.TestCase):
         exist, no exception should be raised)"""
         self.storage.new(self.model)
         self.storage.save()
-        self.storage.reload()
+        FileStorage.reload(self)
         key = self.model.__class__.__name__ + "." + self.model.id
         self.assertIn(key, FileStorage._FileStorage__objects)
     
